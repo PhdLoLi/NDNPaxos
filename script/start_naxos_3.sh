c49a88e6e2afb6a468a42493630c2b2225688b90
Lijing Wang <ooofreedom@gmail.com>
2016-04-16 19:10:56 +0800

    update scripts

--- script/start_naxos_3.sh
+++ script/start_naxos_3.sh
@@ -15,11 +15,15 @@ do
             for k in $( seq 0 2 )
             do
             echo node${k}
-               nohup ssh -t root@node${k} "cd /home/lijing/NDNPaxos && $program ${k} 3 50 1" &
+               nohup ssh -t root@node${k} "cd /home/lijing/NDNPaxos && ./$program ${k} 3 50 1" &
             done
     #        echo  $client_num 
     #        ssh -t root@node4 "cd /home/lijing/naxos &&  bin/clients $client_num $write_ratio 2"
-            cd /home/lijing/NDNPaxos && bin/clients $client_num $write_ratio        
+            cur_time=`date +"%m%d(%H:%M)"`
+            folder_name="${cur_time}""_5r_node0"
+
+            cd /home/lijing/NDNPaxos &&  bin/clients $client_num $write_ratio && cd results/$program && mkdir $folder_name && mv *.txt $folder_name   #run @node6      
+
             for k in $( seq 0 2 )
             do
                ssh -t root@node${k} "killall -v $program" 
--- script/start_naxos_5.sh
+++ script/start_naxos_5.sh
@@ -16,14 +16,17 @@ do
             for k in $( seq 0 4 )
             do
             echo ssh to node${k}
-               nohup ssh -t root@node${k} "cd /home/lijing/NDNPaxos && $program ${k} 5 50 1" &
+               nohup ssh -t root@node${k} "cd /home/lijing/NDNPaxos && ./$program ${k} 5 50 1" &
             done
     #        echo  $client_num 
     #        ssh -t root@node4 "cd /home/lijing/naxos &&  bin/clients $client_num $write_ratio 2"
     #exe from node* locally
     #        nohup ssh -t root@node7 "cd /home/lijing/naxos &&  bin/clients $client_num $write_ratio 3" &
-    
-            cd /home/lijing/NDNPaxos &&  bin/clients $client_num $write_ratio  #run @node6      
+            cur_time=`date +"%m%d(%H:%M)"`
+            folder_name="${cur_time}""_5r_node0"
+
+            cd /home/lijing/NDNPaxos &&  bin/clients $client_num $write_ratio && cd results/$program && mkdir $folder_name && mv *.txt $folder_name   #run @node6      
+
             for k in $( seq 0 4 )
             do
                ssh -t root@node${k} "killall -v $program" 
