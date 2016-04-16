34545ef8667c2a689928c460aacfb73b14d9dc1c
Lijing Wang <ooofreedom@gmail.com>
2016-04-16 17:47:29 +0800

    update git.sh

--- script/git.sh
+++ script/git.sh
@@ -1,1 +1,1 @@
-cd /home/lijing/NDNPaxos && git stash && git pull && ./waf configure -l info && ./waf && cp bin/naxos ../basic && ./waf clean && ./waf configure -l info - m Q && cp bin/naxos ../quorum && ./waf clean && ./waf configure -l info -m M && ./waf && cp bin/naxos ../multi
+cd /home/lijing/NDNPaxos && git stash && git pull && ./waf configure -l info && ./waf && cp bin/naxos ../basic && ./waf clean && ./waf configure -l info -m Q && cp bin/naxos ../quorum && ./waf clean && ./waf configure -l info -m M && ./waf && cp bin/naxos ../multi
